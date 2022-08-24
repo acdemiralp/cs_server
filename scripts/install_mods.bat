f51f919258d94069773ceda05ce07af6f469e424
acdemiralp <demiralpali@gmail.com>
2022-08-24 20:44:17 +0200

    Revert "Added removed maps to maps.ini. Renamed removed maps mod name."
    This reverts commit 3d7898c08212774a91ce88de3a502a229b95eccb.

--- scripts/install_mods.bat
+++ scripts/install_mods.bat
@@ -5,5 +5,5 @@ tar -xf ..\..\mods\metamod-1.21.1-am.zip
 tar -xf ..\..\mods\amxmodx-1.8.2-base-windows.zip
 tar -xf ..\..\mods\amxmodx-1.8.2-cstrike-windows.zip
 tar -xf ..\..\mods\yapb-4.2.671-windows.zip
-tar -xf ..\..\mods\cs-removed-maps-1.0.zip
+tar -xf ..\..\mods\cs-official-maps-1.0.zip
 cd ..\..\scripts
